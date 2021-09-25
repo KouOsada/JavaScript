365e0ead96ca91454d3be21c1a249acffd5341b9
KouOsada <kouosada.0930@gmail.com>
2021-09-25 14:41:41 +0900

    [Add]繰り返し処理

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
-let orange = 120;
-let apple = 100;
+// 条件分岐
+// let orange = 120;
+// let apple = 100;
 
-if(orange < apple){
-  alert('みかんの値段がりんごより安い');
-}else if(orange == apple){
-  alert('みかんとりんごが同じ値段');
-}else{
-  alert('みかんの値段がりんごより高い');
-}
+// if(orange < apple){
+//   alert('みかんの値段がりんごより安い');
+// }else if(orange == apple){
+//   alert('みかんとりんごが同じ値段');
+// }else{
+//   alert('みかんの値段がりんごより高い');
+// }
+// 条件分岐ここまで
+
+
+
+// 繰り返し処理
+// ①while文
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while(num < max){
+//   num = num*2;
+//   count = count+1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+// ②do...whle文
+// do{
+//   最低1回は行われる処理
+// }while(条件式);
+
+// ③for文
+// let i;
+// let num = 0;
+
+// for(i = 1; i < 11; i++){
+//   num = num + i;
+// }
+
+// alert('1から10まで足し算した結果は' + num + 'です')
+// 繰り返し処理ここまで
+
